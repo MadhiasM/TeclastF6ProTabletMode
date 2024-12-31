5ef42169becb3890febd11d373016907ef850238
MadhiasM <mathias.moog1001@googlemail.com>
2024-12-31 16:50:45 +0100

    Adding reasoning for using while with nanosleep instead of event-driver approach or polling/inotify

--- tablet_mode.c
+++ tablet_mode.c
@@ -165,7 +165,8 @@ int main() {
 
 
     // Event-driven approach is not possible since HW/SW triggers are not supported by the sensor driver (/sys/bus/iio/devices/iio:device0/trigger/current_trigger is empty)
-    // poll() or inotify() are not efficient since they will run on change of raw acceleration values which are very volatile due to noise or slightest laptop movements. Hence a while-loop with 1 s nanosleep is used.
+    // poll() or inotify() are not efficient since they will run on change of raw acceleration values which are very volatile due to noise or slightest laptop movements. Hence a while-loop with 1 s nanosleep is used to avoid busy-wait.
+
     // TODO:
     // READ MOUNT MATRIX HERE
     // THEN BASED ON READINGS: READ 2 DIMENSIONS NEEDED (in our case: Y and Z of both (after correction))
